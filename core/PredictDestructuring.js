53385cc3320b117a26c1536c1a5832fc2606d01a
chhuonso <103080113+chhuonso@users.noreply.github.com>
2022-08-29 13:59:00 -0700

    Convert to Arrow

--- core/PredictDestructuring.js
+++ core/PredictDestructuring.js
@@ -34,33 +34,33 @@
 // Prediction: password is not undefined
 // ANSWER: undefined
 
-const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
-const [,first] = numbers;
-const [,,,second] = numbers;
-const [,,,,,,,,third] = numbers;
-//Predict the output
-console.log(first == second);
-console.log(first);
-console.log(second);
-console.log(first == third);
-console.log(third);
+// const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+// const [,first] = numbers;
+// const [,,,second] = numbers;
+// const [,,,,,,,,third] = numbers;
+// //Predict the output
+// console.log(first == second);
+// console.log(first);
+// console.log(second);
+// console.log(first == third);
+// console.log(third);
 
 // prediction: false, true 
 // Answer: same
 
 
-const lastTest = {
-    key: 'value',
-    secondKey: [1, 5, 1, 8, 3, 3]
-}
-const { key } = lastTest;
-const { secondKey } = lastTest;
-const [ ,willThisWork] = secondKey;
-//Predict the output
-console.log(key);
-console.log(secondKey);
-console.log(secondKey[0]);
-console.log(willThisWork);
+// const lastTest = {
+//     key: 'value',
+//     secondKey: [1, 5, 1, 8, 3, 3]
+// }
+// const { key } = lastTest;
+// const { secondKey } = lastTest;
+// const [ ,willThisWork] = secondKey;
+// //Predict the output
+// console.log(key);
+// console.log(secondKey);
+// console.log(secondKey[0]);
+// console.log(willThisWork);
 
 // Prediction:
 // value
