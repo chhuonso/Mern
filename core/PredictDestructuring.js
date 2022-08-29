b8d3a07d5ece2740e3c923d218010ab54242422a
chhuonso <103080113+chhuonso@users.noreply.github.com>
2022-08-29 12:28:54 -0700

    Predict Destructuring

---
+++ core/PredictDestructuring.js
@@ -0,0 +1,69 @@
+// const cars = [ 'Tesla', 'Mercedes', 'Honda']
+// const [ randomCar ] = cars
+// const [,, otherRandomCar ] = cars 
+
+// console.log(randomCar)
+// console.log(otherRandomCar)
+// Prediction: Tesla, Mercedes
+// Answer: Same answer
+
+
+// const employee = {
+//     name: 'Elon',
+//     age: 47,
+//     company: 'Tesla'
+// }
+// const { name: otherName } = employee;
+// //Predict the output
+// console.log(name);
+// console.log(otherName);
+// // Prediction: name is not defined
+// // answer: name is not defined
+
+
+// const person = {
+//     name: 'Phil Smith',
+//     age: 47,
+//     height: '6 feet'
+// }
+// const password = '12345';
+// const { password: hashedPassword } = person;  
+// //Predict the output
+// console.log(password);
+// console.log(hashedPassword);
+// Prediction: password is not undefined
+// ANSWER: undefined
+
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first == second);
+console.log(first);
+console.log(second);
+console.log(first == third);
+console.log(third);
+
+// prediction: false, true 
+// Answer: same
+
+
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
+
+// Prediction:
+// value
+// [1, 5, 1, 8, 3, 3]
+// 1
+// 5
