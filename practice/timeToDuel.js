8313433ee2e2c1cea030dee0cc5d9a6fe54111b4
chhuonso <103080113+chhuonso@users.noreply.github.com>
2022-08-29 21:34:29 -0700

    added in line 35 -37
    added  line 55 - 56

--- practice/timeToDuel.js
+++ practice/timeToDuel.js
@@ -19,6 +19,9 @@ class Unit extends Card {
             target.res = 0;
         }
     }
+    showStats() {
+        console.log("Current stats: " + this.res);
+    }
 }
 
 
@@ -29,7 +32,25 @@ class Effect extends Card {
         this.stat = stat;
         this.mag = mag;
     }
-
+    enhance(unit) {
+        console.log(this.text);
+        unit.res += this.mag;
+    }
 
 }
 
+// Unit cards
+const unit1 = new Unit("Red Belt Ninha", 3, 3, 4);
+const unit2 = new Unit("Black Belt Ninha", 4, 5, 4);
+
+// Effect cards
+const effect1 = new Effect("Hard Algorithm", 2, "increase target's resilience by 3", "resilience", +3);
+const effect2 = new Effect("Unhandled Promise Rejection", 1, "reduce target's resilience by 2", "resilience",-2);
+const effect3 = new Effect("Pair Programming", 3, "    increase target's power by 2" , "power", +2);
+
+
+
+unit1.attack(unit2);
+unit2.showStats();
+effect3.enhance(unit2);
+unit2.showStats();
