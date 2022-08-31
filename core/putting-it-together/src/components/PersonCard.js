018f26f36433b2b92fd2665f30ee1205db691aef
chhuonso <103080113+chhuonso@users.noreply.github.com>
2022-08-30 23:08:40 -0700

    Putting it together

---
+++ core/putting-it-together/src/components/PersonCard.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class PersonCard  extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            firstName: this.props['firstName'],
+            lastName: this.props['lastName'],
+            age: this.props['age'],
+            hairColor: this.props['hairColor']
+        }
+    }
+    render(){
+        return <div>
+            <h1>{this.state.lastName}, {this.state.firstName}</h1>
+            <p>Age: {this.state.age}</p>
+            <p>Hair Color: {this.state.hairColor}</p>
+            <button onClick={()=>this.setState({age:this.state.age+1})}>Birthday Button for {this.state.firstName} {this.state.lastName}</button>
+        </div>
+    }
+
+}
+
+export default PersonCard;
