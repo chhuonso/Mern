296b0576a135babbac06ec223227234f823a0df9
chhuonso <103080113+chhuonso@users.noreply.github.com>
2022-08-30 23:21:23 -0700

    also added from the video option

--- core/putting-it-together/src/components/PersonCard.js
+++ core/putting-it-together/src/components/PersonCard.js
@@ -7,12 +7,25 @@ class PersonCard  extends Component {
             firstName: this.props['firstName'],
             lastName: this.props['lastName'],
             age: this.props['age'],
-            hairColor: this.props['hairColor']
+            hairColor: this.props['hairColor'],
+
+
+            // in the video option 
+            // this.state = {
+            // age: this.props.age
+            //}
         }
     }
+// in the video option
+    // changeAge = () => {
+//      this.setState({age: this.state.age + 1})
+    // }
+
+
     render(){
         return <div>
             <h1>{this.state.lastName}, {this.state.firstName}</h1>
+            {/* also did the same to update age to state */}
             <p>Age: {this.state.age}</p>
             <p>Hair Color: {this.state.hairColor}</p>
             <button onClick={()=>this.setState({age:this.state.age+1})}>Birthday Button for {this.state.firstName} {this.state.lastName}</button>
