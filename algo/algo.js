5a72119645eb492d9e6a5028026f47b8ddb00516
chhuonso <103080113+chhuonso@users.noreply.github.com>
2022-08-31 09:59:14 -0700

    selection sort

--- algo/algo.js
+++ algo/algo.js
@@ -20,3 +20,25 @@ function bubbleSort(arr){
 console.log(bubbleSort([5,3,4,2,1]))
 
 // console.log(bubbleSort([100,10,1,-50,-1,20,50,0]))
+
+
+
+
+// Selected Sort
+function selectionSort(arr){
+    for(let i = 0; i < arr.length; i++){
+        let min = i ;
+        for(let j = i + 1; j < arr.length; j++){
+            if( arr[j] < arr[min]){
+                min = j
+            }
+        }
+        let temp = arr[i];
+        arr[i] = arr[min];
+        arr[min] = temp;
+    }
+    return arr;
+}
+
+
+console.log(selectionSort([3,2,9,5,1,4,8]))
