bd14dc52a26fa1863714dc5e6c07fd0407fab27d
chhuonso <103080113+chhuonso@users.noreply.github.com>
2022-08-30 23:24:12 -0700

    forgot to add the onClick

--- core/putting-it-together/src/components/PersonCard.js
+++ core/putting-it-together/src/components/PersonCard.js
@@ -28,6 +28,7 @@ class PersonCard  extends Component {
             {/* also did the same to update age to state */}
             <p>Age: {this.state.age}</p>
             <p>Hair Color: {this.state.hairColor}</p>
+            {/* onClick= {this.changeAge} */}
             <button onClick={()=>this.setState({age:this.state.age+1})}>Birthday Button for {this.state.firstName} {this.state.lastName}</button>
         </div>
     }
