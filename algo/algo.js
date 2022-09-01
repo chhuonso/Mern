a442d4fba7557e860b5ad25f34a7e3ce41253697
chhuonso <103080113+chhuonso@users.noreply.github.com>
2022-09-01 15:35:10 -0700

    insertionSort

--- algo/algo.js
+++ algo/algo.js
@@ -24,7 +24,7 @@ console.log(bubbleSort([5,3,4,2,1]))
 
 
 
-// Selected Sort
+// Selected Sort WK1D3
 function selectionSort(arr){
     for(let i = 0; i < arr.length; i++){
         let min = i ;
@@ -42,3 +42,24 @@ function selectionSort(arr){
 
 
 console.log(selectionSort([3,2,9,5,1,4,8]))
+
+
+
+//Insert sort W1D4
+
+//  still need to fix this code 
+function insertionSort(arr){
+    for(let i = 1; i < arr.length; i++){
+        let temp = arr[i];
+        for(let j = i - 1; j >= -1; j--){
+            if(temp < arr[j]){
+                arr[j + 1] = arr[j];
+            } else {
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    return arr
+}
+
+console.log(insertionSort([2,3,5,11,1,6]))
