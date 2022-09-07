18c85c2f5311980fa4321b9d8b170a26a1c8c236
chhuonso <103080113+chhuonso@users.noreply.github.com>
2022-09-06 18:16:49 -0700

    Main

--- practice/route-practice/src/components/Main.jsx
+++ practice/route-practice/src/components/Main.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import { Link } from 'react-router-dom';
+
 
 const Main = () => {
   return (
     <div>
-        <h1>hello</h1>
-        <Link to={"/home"}><h5>Home</h5></Link>
+      <h1>hello</h1>
+      <Link to={"/home"}><h5>Home</h5></Link>
     </div>
   )
 }
