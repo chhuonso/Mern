0346e539b11a6f16b95f648d4178efdf9a7c35ee
chhuonso <103080113+chhuonso@users.noreply.github.com>
2022-09-08 02:30:49 -0700

    App.js

---
+++ core/luke-api--walker/src/App.js
@@ -0,0 +1,21 @@
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import People from './components/People';
+import Form from './components/Form';
+import Planets from './components/Planets';
+function App() {
+  return (
+    <div className="App">
+      <h1>Star Wars</h1>
+      <Form />
+      <Routes>
+        <Route path='/' element={<h1>Welcome to the Darkside</h1>}/>
+        <Route path='/people/:inputId' element={<People />}/>
+        <Route path='/planets/:inputId' element={<Planets />}/>
+
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
